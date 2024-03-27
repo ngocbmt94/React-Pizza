28b50e4c5027e0748f722084d551b6b124ea2be9
bichngoc94 <ngocbmt94@gmail.com>
2024-03-27 15:16:08 +0700

    update logo

--- src/UI-components/Header.jsx
+++ src/UI-components/Header.jsx
@@ -5,7 +5,9 @@ import UserName from '../features/users/UserName';
 function Header() {
   return (
     <header className="flex items-center justify-between bg-teals-900 px-6 py-4 text-teals-100 shadow-md">
-      <Link to="/">FAST PIZZA CO.</Link>
+      <Link to="/">
+        <img src="logo-new.svg" alt="logo" className="w-[65px]" />
+      </Link>
       <SearchOrder />
       <UserName />
     </header>
